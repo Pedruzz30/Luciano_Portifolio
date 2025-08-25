6444c4056a5eb80b87a8aed251e75ec3abab8496
Pedruzz30 <pedrohhenriquepimenta224@gmail.com>
2025-08-25 18:17:21 -0300

    mais ajustes

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const toggleBtn = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
 
-toggleBtn.addEventListener('click', () => {
-  menu.classList.toggle('open');
-});
-
-menu.querySelectorAll('a').forEach(link => {
-  link.addEventListener('click', () => {
-    menu.classList.remove('open');
+if (toggleBtn && menu) {
+  toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('open');
   });
-});
 
-const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+  menu.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      menu.classList.remove('open');
+    });
+  });
+}
+
+const path = window.location.pathname;
+const currentPage = path === '/' ? 'index.html' : path.split('/').pop();
 document.querySelectorAll('.menu a').forEach(link => {
   const href = link.getAttribute('href');
   if (href.endsWith(currentPage)) {
